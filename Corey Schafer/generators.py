5d2f1f5e5204749917dfc2c8ad76623300605f4d
Monarc <francomatiaslujan23@gmail.com>
2021-11-29 13:50:10 -0300

    generators and a slight refactor

---
+++ Corey Schafer/generators.py
@@ -0,0 +1,21 @@
+# the reason we use generators over lists is that it is much more readable
+# instead of having a new list, appending the result and returning
+# we YIELD each item just ONCE and that's it
+
+# the lines commented are the ones we're supposed to use if this was a for loop
+# and not a generator
+def square_numbers(nums):
+    # result = [] we don't need to instantiate the list
+    for i in nums:
+        yield (i*i)  # result.append(i*i) is the for loop way
+    # return result we don't need the return since we're using the yield
+
+
+my_nums = square_numbers([1, 2, 3, 4, 5])
+# my_nums = [x*x for x in [1, 2, 3, 4, 5]] # this is a list comprehension
+                                           # it does the same thing as the def above if it was a for loop
+# my_nums = (x*x for x in [1, 2, 3, 4, 5]) # AND THIS is the way to have it as a generator, we switch the [] for ()
+
+print(my_nums.__next__())  # prints [1, 4, 9, 16, 25]
+# you can cast this generator into a list but it loses it's purpose of being a
+# generator that actually helps performance
